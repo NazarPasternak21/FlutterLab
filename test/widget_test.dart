f0be14389b242a0c7b676709a21b610a12fed053
NazarPasternak21 <nazarpasternak00@gmail.com>
2025-03-14 22:37:22 +0200

    Виправлення помилок 3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:your_project_name/main.dart'; 
+import 'package:package:myproject/main.dart'; 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
