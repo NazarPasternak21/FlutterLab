a9e012fe6500688caeb998fda0bf1d8fe5379169
NazarPasternak21 <nazarpasternak00@gmail.com>
2025-03-26 13:13:59 +0200

    fix comments 2.0

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:my_project/widgets/app_state.dart';
+import 'package:my_project/services/app_state.dart';
 import 'package:my_project/screens/profile_screen.dart';
 
 void main() {
